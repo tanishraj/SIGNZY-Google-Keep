8a9462af37ee2d9d9639289bea38277fee11c2c9
tanishraj <tanishraj.quitegrowth@gmail.com>
2020-05-12 08:23:57 +0530

    List item component is created.

---
+++ src/components/notes-list/notes-list.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./notes-list.style.scss";
+
+const NotesList = () => {
+  return (
+    <div className="notes-list">
+      <h1>All the List will come here</h1>
+    </div>
+  );
+};
+
+export default NotesList;
